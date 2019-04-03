75316aa9af9800bc31b3f78abcea158e1fabf9cd
Ellen <ellencshen@gmil.com>
2019-04-03 15:28:22 -0700

    Assignment 2588 To Do App: completed (corrected)

--- app.js
+++ app.js
@@ -3,26 +3,59 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
+  //add task to list
+
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
     //get the text
     let title = newToDoText.value;
 
-    // create a new li
+    //create a primary content span for the text
+    let titlespan = document.createElement('span');
+    titlespan.className = 'mdl-list__item-primary-content';
+    titlespan.textContent = title;
+
+    //create a new li
     let newLi = document.createElement('li');
+    newLi.className = 'mdl-list__item';
 
-    // create a new input
+    //attach the primary content span to the new li
+    newLi.appendChild(titlespan);
+
+    //create a new input
     let checkbox = document.createElement('input');
+    checkbox.className = 'mdl-checkbox__input';
+    checkbox.id = 'list-checkbox-1';
+    checkbox.type = 'checkbox';
 
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
+    //create a new label for the input
+    let checkboxlabel = document.createElement('label');
+    checkboxlabel.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
+    checkboxlabel.setAttribute('for','list-checkbox-1');
+    checkboxlabel.appendChild(checkbox);
 
-    // set the title
-    newLi.textContent = title;
+    //create a secondary action span for the label and input
+    let checkboxspan = document.createElement('span');
+    checkboxspan.className = 'mdl-list__item-secondary-action';
+    checkboxspan.appendChild(checkboxlabel);
 
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
+    //add the secondary action span to the new li
+    newLi.appendChild(checkboxspan);
+
+    //create a new delete button
+    let DeleteButton = document.createElement('button');
+    DeleteButton.className = 'mdl-button mdl-js-button';
+    let DeleteButtonText = document.createTextNode('Delete');
+    DeleteButton.appendChild(DeleteButtonText);
+
+    //create a secondary action span for the delete button
+    let deletespan = document.createElement('span');
+    deletespan.className = 'mdl-list__item-secondary-action';
+    deletespan.appendChild(DeleteButton);
+
+    //attach the delete button to the new li
+    newLi.appendChild(deletespan);
 
     //attach the li to the ul
     toDoList.appendChild(newLi);
@@ -30,8 +63,14 @@ function onReady() {
     //empty the input
     newToDoText.value ='';
 
-  });
-}
+    //delete task from list
+    DeleteButton.addEventListener('click', function(e) {
+    e.currentTarget.parentNode.parentNode.remove();
+    });
+
+    });
+
+  };
 
 window.onload = function() {
   onReady();
